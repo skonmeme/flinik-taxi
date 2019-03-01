78a3cd96e6b02b300c3ca9bf9a6e3d41212ad633
Sung Gon Yi <skonmeme@gmail.com>
2019-03-01 22:21:33 +0900

    Type casting is fixed by using asInstanceOf method.

--- src/main/scala/com/skt/skon/taxi/RulesEngine.scala
+++ src/main/scala/com/skt/skon/taxi/RulesEngine.scala
@@ -10,8 +10,9 @@ import com.skt.skon.taxi.utils.TaxiExecutionEnvironment
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.flink.api.common.functions.FlatMapFunction
 import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.api.common.state.{MapStateDescriptor, ValueStateDescriptor}
+import org.apache.flink.api.common.state.{MapStateDescriptor, ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala._
+import org.apache.flink.runtime.state.KeyedStateFunction
 import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.util.Collector
@@ -23,6 +24,14 @@ import scala.util.Try
 
 object RulesEngine extends LazyLogging {
   case class TaxiFinder(condition: String)
+  case class TaxiRideStatus(rideId: Long, taxiId: Long, driverId: Long, isStart: Boolean, longitude: Float, latitude: Float, timestamp: Long, passengerCount: Short)
+
+  object To {
+    def taxiRideStatus(ride: TaxiRide): TaxiRideStatus = {
+      if (ride.isStart) TaxiRideStatus(ride.rideId, ride.taxiId, ride.driverId, ride.isStart, ride.startLon, ride.startLat, ride.startTime.getMillis, ride.passengerCnt)
+      else TaxiRideStatus(ride.rideId, ride.taxiId, ride.driverId, ride.isStart, ride.endLon, ride.endLat, ride.endTime.getMillis, ride.passengerCnt)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     val servingSpeedFactor = 60   // events of 1 minutes are served in 1 second
@@ -66,32 +75,39 @@ object RulesEngine extends LazyLogging {
       .process(new TaxiFinderProcessFunction)
       .setParallelism(4)
 
-    TaxiExecutionEnvironment.print(distanceStream, "taxi-response", new TaxiSchema[TaxiRide], producerProperties)
+    TaxiExecutionEnvironment.print(distanceStream, "taxi-response", new TaxiSchema[TaxiRideStatus], producerProperties)
 
     env.execute("Rules Engine")
   }
 
-  class TaxiFinderProcessFunction extends KeyedBroadcastProcessFunction[Long, TaxiRide, TaxiFinder, TaxiRide] {
+  class TaxiFinderProcessFunction extends KeyedBroadcastProcessFunction[Long, TaxiRide, TaxiFinder, TaxiRideStatus] {
     private val rideStateDiscriptor =  new ValueStateDescriptor[TaxiRide]("taxi-ride", classOf[TaxiRide])
-    private lazy val rideState = getRuntimeContext.getState(rideStateDescriptor)
+    private lazy val rideState = getRuntimeContext.getState(rideStateDiscriptor)
 
-    override def processElement(ride: TaxiRide, ctx: KeyedBroadcastProcessFunction[Long, TaxiRide, TaxiFinder, TaxiRide]#ReadOnlyContext, out: Collector[TaxiRide]): Unit = {
+    override def processElement(ride: TaxiRide, ctx: KeyedBroadcastProcessFunction[Long, TaxiRide, TaxiFinder, TaxiRideStatus]#ReadOnlyContext, out: Collector[TaxiRideStatus]): Unit = {
       rideState.update(ride)
     }
 
-    override def processBroadcastElement(value: TaxiFinder, ctx: KeyedBroadcastProcessFunction[Long, TaxiRide, TaxiFinder, TaxiRide]#Context, out: Collector[TaxiRide]): Unit = {
+    override def processBroadcastElement(value: TaxiFinder, ctx: KeyedBroadcastProcessFunction[Long, TaxiRide, TaxiFinder, TaxiRideStatus]#Context, out: Collector[TaxiRideStatus]): Unit = {
       ctx.applyToKeyedState(rideStateDiscriptor, new KeyedStateFunction[Long, ValueState[TaxiRide]] {
-        override def process() {
-          val ee = new ExpressionEvaluator()
-          ee.setParameters(
-            Array("watermark", "ride.rideId", "ride.taxiId", "ride.driverId", "ride.isStart",
-              "ride.startTime", "ride.endTime", "ride.passengerCnt",
-              "ride.startLon", "ride.startlat", "ride.endLon", "ride.endLat"),
-            Array(classOf[Long], classOf[Long], classOf[Long], classOf[Long], classOf[Boolean],
-              classOf[String], classOf[String], classOf[Short],
-              classOf[Float], classOf[Float], classOf[Float], classOf[Float]))
-          ee.setExpressionType(classOf[Boolean])
-          if (ee.cook(value.condition)) out.collect(ride)
+        val a: Array[Class[_]] = Array(classOf[Long])
+        override def process(key: Long, rideState: ValueState[TaxiRide]): Unit = {
+          val ride = rideState.value
+          val watermark = ctx.currentWatermark
+
+          if (ride !=null) {
+            Try {
+              val ee = new ExpressionEvaluator()
+              ee.setParameters(Array("watermark", "ride"), Array(classOf[Long], classOf[TaxiRide]).asInstanceOf[Array[Class[_]]])
+              ee.setExpressionType(classOf[Boolean])
+              ee.cook(value.condition.toString)
+              val result = ee.evaluate(Array(watermark, ride).asInstanceOf[Array[AnyRef]]).asInstanceOf[Boolean]
+              if (result) out.collect(To.taxiRideStatus(ride))
+            }.recover {
+              case e: Exception => logger.debug(e.getMessage)
+              case _ => logger.error("Unknown error is occurred.")
+            }
+          }
         }
       })
     }
